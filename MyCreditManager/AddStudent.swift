f7700bed75acb9c7d68867dd7ebcb6eda6f5313b
Mi Yeon Lee <immioriii@gmail.com>
2022-11-17 23:44:13 +0900

    feat: 학생추가 기능을 할 protocol, class 추가

--- MyCreditManager/AddStudent.swift
+++ MyCreditManager/AddStudent.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol AddStudentProtocol {
+    func addStudent()
+}
+class AddStudent : AddStudentProtocol {
+    func addStudent() {
+        guard let input = readLine() else { return }
+        if input == "" {
+            print("입력안함")
+        } else {
+            //입력함
+        }
+    }
+}
