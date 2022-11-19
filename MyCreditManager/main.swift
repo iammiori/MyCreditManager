87e2e0fb6263a1bf4e43ae5ee897be47e75c9767
Mi Yeon Lee <immioriii@gmail.com>
2022-11-19 23:58:42 +0900

    feat: main 파일 추가

---
+++ MyCreditManager/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  MyCreditManager
+//
+//  Created by miori Lee on 2022/11/16.
+//
+
+import Foundation
+
+//FeatureEnum.allCases.forEach { feature in
+//    print("\(feature.rawValue)" , terminator: " ")
+//}
+//
+//AddStudent().addStudent()
+//
+//private var choice = readLine()
+//if let choice = choice {
+//    print(choice)
+//}
