b1d8456ac8fe94d23b8677773d935ea10d9f8629
Mi Yeon Lee <immioriii@gmail.com>
2022-11-25 23:38:46 +0900

    refac: grade 모델과 grademanage 모델 추가

---
+++ MyCreditManager/Model/Grade.swift
@@ -0,0 +1,13 @@
+//
+//  Grade.swift
+//  MyCreditManager
+//
+//  Created by miori Lee on 2022/11/25.
+//
+
+import Foundation
+
+struct Grade {
+    var subject : String
+    var grade : Double
+}
---
+++ MyCreditManager/Model/GradeManage.swift
@@ -0,0 +1,13 @@
+//
+//  GradeManage.swift
+//  MyCreditManager
+//
+//  Created by miori Lee on 2022/11/25.
+//
+
+import Foundation
+
+struct GradeManage {
+    let name: String
+    var credit : [Grade?]
+}
