209cecbfee622f782ac9b3d79ede15bd3d8f694a
Mi Yeon Lee <immioriii@gmail.com>
2022-11-28 18:55:58 +0900

    feat: 문구 구조체 안에 text 수정

--- MyCreditManager/Strings.swift
+++ MyCreditManager/Strings.swift
@@ -11,6 +11,7 @@ struct MainStream {
     static let startInfo = "원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료"
     static let off = "프로그램을 종료합니다..."
     static var inputErr = "입력이 잘못되었습니다. 다시 확인해주세요."
+    static var wrongErr = "뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요."
 }
 
 struct AddStudent {
@@ -29,7 +30,7 @@ struct AddScore {
 }
 
 struct DeleteScore {
-    static let notice = "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차레로 작성해주세요"
+    static let notice = "성적을 삭제할 학생의 이름, 과목 이름을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예 Mickey Swift"
 }
 
 struct ShowAverage {
