8e0415491259e80fe71baab6d6de1e6895da82a9
Mi Yeon Lee <immioriii@gmail.com>
2022-12-01 23:58:11 +0900

    refac: SRP원칙에 근거 학생클래스에서 해야할일 분리

---
+++ MyCreditManager/Model/Student.swift
@@ -0,0 +1,17 @@
+//
+//  Student.swift
+//  MyCreditManager
+//
+//  Created by miori Lee on 2022/12/01.
+//
+
+import Foundation
+
+class Student {
+    private let name: String
+    private let grades: [Grade] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+}
